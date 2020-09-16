bbff908540993e7791e8155ff602394c4e0c0ac1
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-09-16 11:06:49 +0200

    Added domæne

--- Server.js
+++ Server.js
@@ -1,9 +1,14 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+const port = process.env.PORT || 80;
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
+    console.log(q);
     var filename = '.' + q.pathname;
+    if(q.pathname="/"){
+        filename='./index.html'
+    }
     fs.readFile(filename, function (err, data) {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
@@ -13,4 +18,4 @@ http.createServer(function (req, res) {
         res.write(data);
         return res.end();
     });
-}).listen(8080);
+}).listen(port);
